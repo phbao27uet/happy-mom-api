4c0fdb89608dd536bb1d73647e2df48ffb9e6d37
baopham64uete <baopq@solashi.com>
2024-10-08 13:28:20 +0900

    feat: :sparkles: admin groups

---
+++ src/shared/schemas/messages/index.ts
@@ -0,0 +1,5 @@
+export const ERROR_MESSAGE = {
+  requiredError: 'Trường này không được để trống',
+  maxCharacters: 'Trường này không được vượt quá 255 ký tự',
+  max1024Characters: 'Trường này không được vượt quá 1024 ký tự',
+}
