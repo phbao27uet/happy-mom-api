b3a3a4c9b0a59b7d0f5d34cdf93ef86a38f529d8
baopham64uete <baopq@solashi.com>
2024-10-08 13:28:20 +0900

    feat: :sparkles: admin groups

--- src/models/groups/groups.service.ts
+++ src/models/groups/groups.service.ts
@@ -1,16 +1,22 @@
-import { DefaultFindAllQueryDto } from '@models/base';
-import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
-import { PrismaService } from 'src/shared/prisma/prisma.service';
+import { DefaultFindAllQueryDto } from '@models/base'
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { Prisma } from '@prisma/client'
+import { PrismaService } from 'src/shared/prisma/prisma.service'
+import { CreateGroupDto, UpdateGroupDto } from './dto'
 
 @Injectable()
 export class GroupsService {
   constructor(private readonly prisma: PrismaService) {}
 
   async findAll(defaultFindAllQuery: DefaultFindAllQueryDto) {
-    const { perPage = 20, page = 1 } = defaultFindAllQuery;
+    const { perPage = 20, page = 1, searchOne } = defaultFindAllQuery
 
-    const where: Prisma.GroupWhereInput = {};
+    const where: Prisma.GroupWhereInput = {
+      name: {
+        contains: searchOne,
+        mode: 'insensitive',
+      },
+    }
 
     const [total, data] = await Promise.all([
       this.prisma.group.count({
@@ -24,7 +30,7 @@ export class GroupsService {
         skip: page && perPage ? (page - 1) * perPage : undefined,
         take: page && perPage ? perPage : undefined,
       }),
-    ]);
+    ])
 
     return {
       data: data,
@@ -34,16 +40,77 @@ export class GroupsService {
         total: total ?? 0,
         totalPages: Math.ceil((total ?? 0) / perPage),
       },
-    };
+    }
+  }
+
+  async create(createDto: CreateGroupDto) {
+    return this.prisma.group.create({
+      data: {
+        ...createDto,
+      },
+    })
+  }
+
+  async findOne(id: string) {
+    const res = await this.prisma.group.findUnique({
+      where: {
+        id: id,
+      },
+    })
+
+    if (!res) {
+      throw new NotFoundException('Nhóm không tồn tại')
+    }
+
+    return res
+  }
+
+  async update(id: string, updateDto: UpdateGroupDto) {
+    const res = await this.prisma.group.findUnique({
+      where: {
+        id: id,
+      },
+    })
+
+    if (!res) {
+      throw new NotFoundException('Nhóm không tồn tại')
+    }
+
+    return this.prisma.group.update({
+      where: {
+        id: id,
+      },
+      data: {
+        ...updateDto,
+      },
+    })
+  }
+
+  async remove(id: string) {
+    const res = await this.prisma.group.findUnique({
+      where: {
+        id: id,
+      },
+    })
+
+    if (!res) {
+      throw new NotFoundException('Nhóm không tồn tại')
+    }
+
+    return this.prisma.group.delete({
+      where: {
+        id: id,
+      },
+    })
   }
 
   async getOptions() {
-    const groups = await this.prisma.group.findMany({});
+    const groups = await this.prisma.group.findMany({})
 
     return groups.map((group) => ({
       value: group.id,
       label: group.name,
       data: group,
-    }));
+    }))
   }
 }
