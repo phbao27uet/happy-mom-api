3f439d585eae99a31a02ddc8c30a30d74aa4979c
baopham64uete <baopq@solashi.com>
2024-10-16 13:23:41 +0900

    feat(user): :memo: update naming for child

--- src/models/users/users.service.ts
+++ src/models/users/users.service.ts
@@ -6,12 +6,209 @@ import { ElementalType, GetNamingForChildDto, UpdateUserDto } from './dto'
 
 @Injectable()
 export class UsersService {
-  private readonly elementalNames: Record<ElementalType, string[]> = {
-    METAL: ['Khánh', 'Khôi', 'Kim', 'Kiều', 'Khang'],
-    WOOD: ['Trúc', 'Lâm', 'Mộc', 'Mai', 'Linh'],
-    WATER: ['Thủy', 'Hải', 'Giang', 'Triều', 'Ngọc'],
-    FIRE: ['Hỏa', 'Dương', 'Minh', 'Nhật', 'Hồng'],
-    EARTH: ['Địa', 'Sơn', 'Thổ', 'Châu', 'Hoàng'],
+  private readonly elementalNames: Record<
+    ElementalType,
+    {
+      MALE: string[]
+      FEMALE: string[]
+    }
+  > = {
+    METAL: {
+      MALE: [
+        'Chung',
+        'Cương',
+        'Doãn',
+        'Hữu',
+        'Khanh',
+        'Luyện',
+        'Nghĩa',
+        'Nguyên',
+        'Nhâm',
+        'Phong',
+        'Thăng',
+        'Thắng',
+        'Thế',
+        'Thiết',
+        'Tiền',
+        'Văn',
+      ],
+      FEMALE: [
+        'Ái',
+        'Dạ',
+        'Đoan',
+        'Hân',
+        'Hiền',
+        'Hiện',
+        'Mỹ',
+        'Ngân',
+        'Nhi',
+        'Phượng',
+        'Tâm',
+        'Trang',
+        'Vân',
+        'Vi',
+        'Xuyến',
+      ],
+    },
+    WOOD: {
+      MALE: [
+        'Khôi',
+        'Bách',
+        'Bản',
+        'Bính',
+        'Bình',
+        'Cung',
+        'Đỗ',
+        'Đông',
+        'Hộ',
+        'Khôi',
+        'Kỳ',
+        'Lâm',
+        'Nam',
+        'Nhân',
+        'Phúc',
+        'Phước',
+        'Quảng',
+        'Quý',
+        'Tùng',
+        'Duy',
+      ],
+      FEMALE: [
+        'Bạch',
+        'Chi',
+        'Cúc',
+        'Đào',
+        'Giao',
+        'Hạnh',
+        'Huệ',
+        'Hương',
+        'Lam',
+        'Lan',
+        'Lê',
+        'Liễu',
+        'Lý',
+        'Mai',
+        'Phương',
+        'Quỳnh',
+        'Sa',
+        'Sâm',
+        'Thảo',
+        'Thư',
+        'Tích',
+        'Tiêu',
+        'Trà',
+        'Trúc',
+        'Vị',
+        'Xuân',
+      ],
+    },
+    WATER: {
+      MALE: [
+        'Võ',
+        'Hội',
+        'Hải',
+        'Quang',
+        'Nhậm',
+        'Trí',
+        'Trọng',
+        'Hoàn',
+        'Hồ',
+        'Hà',
+        'Nhâm',
+        'Nhuận',
+        'Luân',
+        'Dư',
+        'Tiến',
+        'Toàn',
+        'Cung',
+        'Hưng',
+        'Quân',
+        'Quyết',
+        'Lưu',
+      ],
+      FEMALE: [
+        'Sương',
+        'Thủy',
+        'Giao',
+        'Giang',
+        'Vũ',
+        'Tuyên',
+        'Thương',
+        'Lệ',
+        'Tiên',
+        'Băng',
+        'Bùi',
+        'Khê',
+        'Kiều',
+        'Hàn',
+        'Loan',
+        'Băng',
+      ],
+    },
+    FIRE: {
+      MALE: [
+        'Luyện',
+        'Quang',
+        'Huân',
+        'Nam',
+        'Thước',
+        'Đăng',
+        'Đức',
+        'Bội',
+        'Nhiên',
+        'Hạ',
+        'Cẩm',
+        'Thanh',
+        'Tiết',
+        'Kim',
+      ],
+      FEMALE: [
+        'Ánh',
+        'Đài',
+        'Hạ',
+        'Cẩm',
+        'Đan',
+        'Cẩn',
+        'Hồng',
+        'Thanh',
+        'Kim',
+        'Tiết',
+        'Bội',
+        'Thu',
+        'Nhiên',
+        'Dung',
+      ],
+    },
+    EARTH: {
+      MALE: [
+        'Sơn',
+        'Côn',
+        'Lý',
+        'Thân',
+        'Thông',
+        'Giáp',
+        'Thạc',
+        'Kiên',
+        'Tự',
+        'Bảo',
+        'Kiệt',
+        'Thành',
+        'Đại',
+      ],
+      FEMALE: [
+        'Châu',
+        'Ngọc',
+        'San',
+        'Cát',
+        'Viên',
+        'Nghiêm',
+        'Châm',
+        'Anh',
+        'Chân',
+        'Diệp',
+        'Bích',
+      ],
+    },
   }
 
   constructor(private readonly prisma: PrismaService) {}
@@ -62,34 +259,38 @@ export class UsersService {
   }
 
   async getNamingForChild(dto: GetNamingForChildDto) {
-    let firstName: string
-    let middleName = ''
-    let firstCharacter: string
+    try {
+      let firstName: string
+      let middleName = ''
+      let firstCharacter: string
 
-    if (dto.type === 'FIRST_CHARACTER' && dto.firstCharacter) {
-      do {
+      if (dto.type === 'FIRST_CHARACTER' && dto.firstCharacter) {
+        do {
+          firstName = faker.person.firstName(
+            dto.gender === 'MALE' ? 'male' : 'female',
+          )
+          const firstNames = firstName.split(' ')
+          firstCharacter = firstNames[firstNames.length - 1][0]
+        } while (
+          firstCharacter.toLowerCase() !== dto.firstCharacter.toLowerCase()
+        )
+      } else if (dto.type === 'ELEMENTAL' && dto.elemental) {
         firstName = faker.person.firstName(
           dto.gender === 'MALE' ? 'male' : 'female',
         )
-        const firstNames = firstName.split(' ')
-        firstCharacter = firstNames[firstNames.length - 1][0]
-      } while (
-        firstCharacter.toLowerCase() !== dto.firstCharacter.toLowerCase()
-      )
-    } else if (dto.type === 'ELEMENTAL' && dto.elemental) {
-      firstName = faker.person.firstName(
-        dto.gender === 'MALE' ? 'male' : 'female',
-      )
 
-      middleName = faker.helpers.arrayElement(
-        this.elementalNames[dto.elemental],
-      )
-    } else {
-      throw new ConflictException('Invalid type')
-    }
+        middleName = faker.helpers.arrayElement(
+          this.elementalNames[dto.elemental][dto.gender],
+        )
+      } else {
+        throw new ConflictException('Invalid type')
+      }
 
-    return {
-      name: `${dto.lastName} ${middleName} ${firstName}`,
+      return {
+        name: `${dto.lastName}${middleName ? ` ${middleName}` : ''} ${firstName}`,
+      }
+    } catch (error) {
+      console.log(error)
     }
   }
 }
